9ed8527cf642a83cd57779b73d862c4b8b764203
PC <PC@LAPTOP-Q5AD0KM4>
2022-09-23 19:59:00 +0300

    Add project files.

---
+++ LoggingDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoggingDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger _logger;
+
+        //The standart way of capturing the category
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IndexModel(ILoggerFactory factory)
+        {
+            _logger = factory.CreateLogger("DemoCategory");
+        }
+
+        public void OnGet()
+        {
+            //logging levels
+            _logger.LogTrace("this a trace log");
+            _logger.LogDebug("this ia a debug log");
+            _logger.LogInformation("this is an information log");
+            _logger.LogWarning("this is a warning log");
+            _logger.LogError("this is an error log");
+            _logger.LogCritical("this is a critical log");
+
+            //_logger.LogError("The server went down temporarily at {Time}",DateTime.UtcNow);
+
+            //try
+            //{
+            //    throw new Exception("You forgot to catch me!");
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    _logger.LogCritical(ex, "There was a bad exception at {Time}", DateTime.UtcNow);
+            //}
+        }
+    }
+        public class LoggingId
+        {
+          public const int DemoCode = 1001;
+        }
+}
+
---
+++ LoggingDemo/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoggingDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           var host =  CreateHostBuilder(args).Build();
+           var logger = host.Services.GetRequiredService<ILogger<Program>>();
+           logger.LogInformation( "The application has started");
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) 
+          { return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((context, logging) => 
+                {
+                    logging.ClearProviders();
+                    logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+                    logging.AddDebug();
+                    logging.AddConsole();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                }); 
+        }
+    }
+}
